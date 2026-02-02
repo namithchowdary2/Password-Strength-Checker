8bf6c3cfbc6af0f6e83f727a51ccce9105c2c996
Namith Chowdary <153614570+namithchowdary2@users.noreply.github.com>
2026-02-02 13:04:33 +0530

    Update Password Strength Checker.py

--- Password Strength Checker.py
+++ Password Strength Checker.py
@@ -5,20 +5,20 @@
 # gives a strength score using math
 # saves the result in a file using OS
 
-import string
+import re
 import math
 import os
 from collections import Counter
-# taking password from user
+# taking password input from user
 password = input("Enter your password: ")
-# checking password strength
-has_upper = any(c.isupper() for c in password) # checks capital letters
-has_lower = any(c.islower() for c in password) # checks small letters
-has_digit = any(c.isdigit() for c in password) # checks numbers
-has_symbol = any(c in string.punctuation for c in password) # checks symbols
-# counting how many conditions are true
+# checking rules using regex
+has_upper = bool(re.search(r"[A-Z]", password))        # checks uppercase
+has_lower = bool(re.search(r"[a-z]", password))        # checks lowercase
+has_digit = bool(re.search(r"[0-9]", password))        # checks numbers
+has_symbol = bool(re.search(r"[!@#$%^&*(),.?\":{}|<>]", password))  # checks symbols
+# counting how many conditions are satisfied
 strength_value = sum([has_upper, has_lower, has_digit, has_symbol])
-# giving score using maths
+# giving score using math
 score = math.ceil((strength_value / 4) * 100)
 # deciding strength level
 if score < 40:
@@ -27,21 +27,21 @@ elif score < 70:
     level = "Medium"
 else:
     level = "Strong"
-# counting each character in password
+# counting each character used in password
 char_count = Counter(password)
-# saving result in file using os
+# saving result into file using os
 folder = "password_reports"
-if not os.path.exists(folder): # create folder if not there
+if not os.path.exists(folder):   # create folder if it doesn't exist
     os.mkdir(folder)
-path = os.path.join(folder, "report_user.txt")
+path = os.path.join(folder, "regex_report.txt")
 file = open(path, "w")
-file.write("User Password: " + password + "\n")
+file.write("Password Entered: " + password + "\n")
 file.write("Character Count: " + str(char_count) + "\n")
 file.write("Strength Score: " + str(score) + "%\n")
 file.write("Strength Level: " + level + "\n")
 file.close()
 # printing output
-print("Password Entered:", password)
+print("Password:", password)
 print("Character Count:", char_count)
 print("Score:", score, "%")
 print("Strength Level:", level)
